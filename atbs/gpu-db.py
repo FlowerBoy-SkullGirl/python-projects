984e58450d995709401cc4dd061d6b035ac96b0e
FlowerBoy-SkullGirl <dmurphyshort@gmail.com>
2022-08-21 03:16:34 -0400

    Add feature: Retrieves every sku and price from single page in for loop

--- atbs/gpu-db.py
+++ atbs/gpu-db.py
@@ -55,14 +55,33 @@ elementSearch.submit()
 
 time.sleep(5)
 
-elementName = browser.find_element(By.XPATH, "//h4[@class='sku-title']//a[1]")
+listEntries = browser.find_elements(By.CLASS_NAME, "sku-item")
+elementPrice = browser.find_elements(By.XPATH, "//div[@class='priceView-hero-price priceView-customer-price']//span[1]")
 
-print("piping data")
 
-print(elementName.text)
+for i in range(len(listEntries)):
 
-elementPrice = browser.find_element(By.XPATH, "//div[@class='priceView-hero-price priceView-customer-price']//span[1]")
+#	elementName = listEntries[i].find_element(By.XPATH, "//h4[@class='sku-title']//a[1]")
+	elementName = listEntries[i].find_element(By.CLASS_NAME, "sku-title")
+	elementName = elementName.find_element(By.XPATH, "a[1]")
 
-print(elementPrice.text)
+	print("piping data")
+
+	print(elementName.text)
+
+#	elementPrice = listEntries[i].find_element(By.XPATH, "div[@class='priceView-hero-price priceView-customer-price']//span[1]")
+
+	print(elementPrice[i].text)
 
 print ("end signal")
+
+browser.close()
+
+#SOUP PLAN
+# gpus = soup.select('class name')
+# for i in range(len(gpus))
+#      gpus[i].get('a')
+#Something like this to get all results on page
+#Can maybe do this with selenium too
+# elements = browser.find_elements(By.ID 'gh-blabla')
+# for i in range(len(elements
