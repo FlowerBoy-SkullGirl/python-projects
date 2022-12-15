13d9ea5f22b5f75de1abf712a14c4172e5f83885
FlowerBoy-SkullGirl <dmurphyshort@gmail.com>
2022-12-15 03:35:49 -0500

    Fix: Input parsing without spaces. Add: Output to show +/- values of roll

--- DiscordBots/DiceRollr/dicerollr.py
+++ DiscordBots/DiceRollr/dicerollr.py
@@ -12,11 +12,58 @@ def roll_dice(dn,ds,da,d_sign,nr):
         total += (da * d_sign)
         totals.append(total)
     if len(totals) == 1:
+        if (da * d_sign) > 1:
+            return_string = "You rolled {}({} +{})!".format(totals[0],totals[0] - da,da)
+            return return_string
+        if d_sign < 0:
+            return_string = "You rolled {}({} {})!".format(totals[0],totals[0] + da,da)
+            return return_string
         return_string = "You rolled {}!".format(totals[0])
         return return_string
     if len(totals) == 2:
         return_string = "You rolled {} and {}!".format(totals[0],totals[1])
+        if (da * d_sign) > 1:
+            return_string += "(+{})".format(da)
+        if d_sign < 0:
+            return_string += "(-{})".format(da)
         return return_string
+
+def parseAlt(commands):
+    diceNum = 1
+    diceSides = 1
+    diceAdd = 0
+    diceSign = 1
+    numRolls = 1
+
+    if 'd' in commands[1]:
+        removeD = commands[1].split('d')
+        try:
+            diceNum = int(removeD[0])
+        except:
+            return help_command
+        if '-' in removeD[1]:
+            diceSign = -1
+            removeSign = removeD[1].split('-')
+            try:
+                diceSides = int(removeSign[0])
+                diceAdd = int(removeSign[1])
+                return roll_dice(diceNum,diceSides,diceAdd,diceSign,numRolls)
+            except:
+                return help_command
+        if '+' in removeD[1]:
+            removeSign = removeD[1].split('+')
+            try:
+                diceSides = int(removeSign[0])
+                diceAdd = int(removeSign[1])
+                return roll_dice(diceNum,diceSides,diceAdd,diceSign,numRolls)
+            except:
+                return help_command 
+        try:
+            diceSides = int(removeD[1])
+        except:
+            return help_command
+
+        return roll_dice(diceNum,diceSides,diceAdd,diceSign,numRolls)
 
 def parseCommand(command):
     #vars to send to roll func
@@ -29,9 +76,15 @@ def parseCommand(command):
     if command.startswith('!roll help') or command == '!roll':
         return help_command
     commands = command.split(' ')
-
+    
+    #Remove chars and extract values, 1 value should be sides of dice
     if len(commands) == 2:
         if 'd' in commands[1]:
+            try:
+                if commands[1][(commands[1].index('d'))-1].isdigit() and commands[1][(commands[1].index('d'))+1].isdigit():
+                    return parseAlt(commands)
+            except:
+                pass
             commands[1] = commands[1].strip('d')
         try:
             diceSides = int(commands[1])
@@ -39,6 +92,7 @@ def parseCommand(command):
             return help_command
         return roll_dice(diceNum,diceSides,diceAdd,diceSign,numRolls)
         
+    #Should be number of dice and sides
     if len(commands) == 3:
         try:
             diceNum = int(commands[1])
@@ -52,6 +106,7 @@ def parseCommand(command):
             return help_command
         return roll_dice(diceNum,diceSides,diceAdd,diceSign,numRolls)
     
+    #Expect number of dice, sides, and a +/-
     if len(commands) == 4:
        try:
            diceNum = int(commands[1])
@@ -75,7 +130,7 @@ def parseCommand(command):
 
        return roll_dice(diceNum,diceSides,diceAdd,diceSign,numRolls)
 
-
+    #Expect advantade/disadvantage
     if len(commands) > 4:
        try:
            diceNum = int(commands[1])
